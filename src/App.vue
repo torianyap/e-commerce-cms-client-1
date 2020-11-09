98c143c8907644bda4f89324588569cd7ccd4327
torianyap <torianyap@gmail.com>
2020-11-09 20:57:31 +0700

    initialize client

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+    <HomePage 
+        v-if="pageName === 'home-page'">
+    </HomePage>
+    <LoginPage
+        v-else-if="pageName === 'login-page'">
+    </LoginPage>
+</div>
+</template>
+
+<script>
+import HomePage from './components/HomePage'
+import LoginPage from './components/LoginPage'
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            pageName: 'home-page'
+        }
+    },
+    components: {
+        HomePage,
+        LoginPage
+    },
+    created() {
+        const access_token = localStorage.access_token
+        if (access_token) {
+
+        } else {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
